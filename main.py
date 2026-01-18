7cf43c389d79fd15c0028e44ac897af6c003a070
TsionMengistu12 <149229052+TsionMengistu12@users.noreply.github.com>
2026-01-18 11:47:07 +0300

    file structure and customtkinter test

---
+++ main.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+##### tester on weather the customtkinter works
+# # ctk.set_appearance_mode("dark")
+# ctk.set_default_color_theme("green")
+
+# app = ctk.CTk()
+# app.geometry("400x400")
+
+# ctk.CTkLabel(app, text="Checking if this works?!").pack(pady=20)
+# app.mainloop()
